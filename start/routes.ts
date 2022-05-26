4350fab269d6517777c5c07cc795489fe3fa2f46
Stuart Ellidge <stu@ellidge.co.uk>
2022-05-26 23:32:20 +0100

    Added login route and test

--- start/routes.ts
+++ start/routes.ts
@@ -23,3 +23,9 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async () => {
   return { hello: 'world' }
 })
+
+Route.post('login', async ({ auth, request }) => {
+  const email = request.input('email')
+  const password = request.input('password')
+  await auth.use('web').attempt(email, password)
+})
